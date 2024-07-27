43dd8de1e97575c7a852186be9d13742d778c4d5
moraiskaua <moraiskaua03@gmail.com>
2024-07-27 18:51:47 -0300

    fix: change mascot images folder

--- app/lesson/components/QuestionBubble.tsx
+++ app/lesson/components/QuestionBubble.tsx
@@ -8,14 +8,14 @@ export const QuestionBubble: React.FC<QuestionBubbleProps> = ({ question }) => {
   return (
     <div className="flex items-center gap-x-4 mb-6">
       <Image
-        src="/mascot.svg"
+        src="/images/mascot.svg"
         alt="Mascot"
         height={60}
         width={60}
         className="hidden lg:block"
       />
       <Image
-        src="/mascot.svg"
+        src="/images/mascot.svg"
         alt="Mascot"
         height={40}
         width={40}
--- components/modals/ExitModal.tsx
+++ components/modals/ExitModal.tsx
@@ -31,7 +31,12 @@ export const ExitModal: React.FC<ExitModalProps> = ({}) => {
       <DialogContent className="max-w-md">
         <DialogHeader>
           <div className="flex items-center w-full justify-center mb-5">
-            <Image src="/mascot_sad.svg" alt="Mascot" width={80} height={80} />
+            <Image
+              src="/images/mascot_sad.svg"
+              alt="Mascot"
+              width={80}
+              height={80}
+            />
           </div>
           <DialogTitle className="text-center font-bold text-2xl">
             Wait, don&apos;t go!
