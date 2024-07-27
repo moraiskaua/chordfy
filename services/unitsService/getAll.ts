b60b2b0e8c89be127fbec27614e40ceeee9921a9
moraiskaua <moraiskaua03@gmail.com>
2024-07-27 04:32:28 -0300

    fix: completed status at lesson button component

--- services/unitsService/getAll.ts
+++ services/unitsService/getAll.ts
@@ -34,6 +34,10 @@ export const getAll = cache(async () => {
 
   const normalizedData = data.map(unit => {
     const lessonsWithCompletedStatus = unit.lessons.map(lesson => {
+      if (lesson.challenges.length === 0) {
+        return { ...lesson, completed: false };
+      }
+
       const allCompletedChallenges = lesson.challenges.every(challenge => {
         return (
           challenge.challengeProgress &&
