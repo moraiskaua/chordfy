ccfcb8662d2d95cf9473c71f86db0a0a218563ba
moraiskaua <moraiskaua03@gmail.com>
2024-07-29 09:39:09 -0300

    fix: change challenge options fields

--- src/app/admin/challengeOption/create.tsx
+++ src/app/admin/challengeOption/create.tsx
@@ -14,13 +14,12 @@ export const ChallengeOptionCreate = () => {
     <Create>
       <SimpleForm>
         <TextInput source="id" label="Id" validate={[required()]} />
-        <TextInput source="text" label="Text" validate={[required()]} />
+        <TextInput source="text" label="Question" validate={[required()]} />
         <BooleanInput
           source="correct"
           label="Correct Option"
           validate={[required()]}
         />
-        <TextInput source="question" label="Question" validate={[required()]} />
         <TextInput source="image" label="Image URL" validate={[required()]} />
         <TextInput source="audio" label="Audio URL" validate={[required()]} />
         <ReferenceInput source="challengeId" reference="challenges" />
