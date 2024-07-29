6692425bbb1fcbb4763927788ec685663c3a50de
moraiskaua <moraiskaua03@gmail.com>
2024-07-29 12:33:46 -0300

    fix: image compare logic

--- src/app/admin/challengeOption/edit.tsx
+++ src/app/admin/challengeOption/edit.tsx
@@ -23,7 +23,7 @@ export const ChallengeOptionEdit = () => {
         <TextInput
           source="image"
           label="Image URL"
-          parse={value => ('null' ? null : value)}
+          parse={value => (value === 'null' ? null : value)}
         />
         <TextInput source="audio" label="Audio URL" validate={[required()]} />
         <ReferenceInput source="challengeId" reference="challenges" />
