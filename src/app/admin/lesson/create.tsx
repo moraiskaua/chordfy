c6a49cf1f9f58324b52c16be97690fd293a6d92e
moraiskaua <moraiskaua03@gmail.com>
2024-07-29 09:30:25 -0300

    fix: remove description lesson field

--- src/app/admin/lesson/create.tsx
+++ src/app/admin/lesson/create.tsx
@@ -12,11 +12,6 @@ export const LessonCreate = () => {
     <Create>
       <SimpleForm>
         <TextInput source="title" label="Title" validate={[required()]} />
-        <TextInput
-          source="description"
-          label="Description"
-          validate={[required()]}
-        />
         <ReferenceInput source="unitId" reference="units" />
         <NumberInput source="order" validate={[required()]} />
       </SimpleForm>
