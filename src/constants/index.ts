ef89e405e68a9cd511dab1e0f793ae4f37669384
moraiskaua <moraiskaua03@gmail.com>
2024-07-29 05:43:27 -0300

    fix: change quests values

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,15 +2,23 @@ export const POINTS_TO_REFIL = 20;
 
 export const quests = [
   {
-    title: 'Earn 20 XP',
-    value: 20,
-  },
-  {
     title: 'Earn 50 XP',
     value: 50,
   },
   {
-    title: 'Earn 70 XP',
-    value: 70,
+    title: 'Earn 100 XP',
+    value: 100,
+  },
+  {
+    title: 'Earn 500 XP',
+    value: 500,
+  },
+  {
+    title: 'Earn 1000 XP',
+    value: 1000,
+  },
+  {
+    title: 'Earn 1500 XP',
+    value: 1500,
   },
 ];
